dc17c41118ce3479585eb73afbf750c2abdc7f53
Kleberson <klebervini49@gmail.com>
2022-01-19 21:03:20 -0300

    🎨 Code Style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 document.querySelector('#formulario').addEventListener('submit', (e) => {
 	e.preventDefault()
 	let valorPeso = document.querySelector('#peso').value,
-		valorAltura = document.querySelector('#alDtura').value,
-		classeRmv = document.querySelectorAll('td');
-	classeRmv.forEach((i) => {
-		i.classList.remove('bad', 'bom', 'medio')
-	})
-
+		valorAltura = document.querySelector('#altura').value,
+		classeRmv = document.querySelectorAll('td'),
+		resultClass;
 	valorPeso = parseFloat(valorPeso);
 	valorAltura = parseFloat(valorAltura);
 
 	let result = valorPeso / (valorAltura ** 2);
 	result = parseFloat(result.toFixed(2))
 
-	let resultClass
+	classeRmv.forEach((i) => {
+		i.classList.remove('bad', 'bom', 'medio')
+	})
+
 	if (result < 18.5) {
 		resultClass = 'bad'
 		document.querySelector('#imc1').classList.add(resultClass)
@@ -33,9 +33,7 @@ document.querySelector('#formulario').addEventListener('submit', (e) => {
 		document.querySelector('#obs3').classList.add(resultClass)
 	}
 	if (result >= 30.0 && result <= 39.9) {
-
 		resultClass = 'bad'
-
 		document.querySelector('#imc4').classList.add(resultClass)
 		document.querySelector('#quali4').classList.add(resultClass)
 		document.querySelector('#obs4').classList.add(resultClass)
@@ -46,6 +44,5 @@ document.querySelector('#formulario').addEventListener('submit', (e) => {
 		document.querySelector('#quali5').classList.add(resultClass)
 		document.querySelector('#obs5').classList.add(resultClass)
 	}
-
 	document.querySelector('#resultado').innerHTML = `<p class='${resultClass}'>Seu IMC É ${result}</p>`
 })
